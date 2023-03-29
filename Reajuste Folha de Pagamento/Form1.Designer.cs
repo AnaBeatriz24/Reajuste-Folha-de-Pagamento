c272db3a3c0081314a4d780c4ca3ee7d4abe2774
Ana Beatriz de Freitas Teixeira <freitasanabeatriz2004@gmail.com>
2023-03-28 23:38:47 -0400

    Adicionar arquivos de projeto.

---
+++ Reajuste Folha de Pagamento/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Reajuste_Folha_de_Pagamento
+{
+    partial class FormLeituraArquivo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvLeitura = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblPercentualDoReajuste = new System.Windows.Forms.Label();
+            this.lblTotalComReajuste = new System.Windows.Forms.Label();
+            this.lblTotalSemReajuste = new System.Windows.Forms.Label();
+            this.ofdListaFuncionarios = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeitura)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Enabled = false;
+            this.txtArquivo.Location = new System.Drawing.Point(72, 21);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(392, 20);
+            this.txtArquivo.TabIndex = 5;
+            this.txtArquivo.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(470, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvLeitura
+            // 
+            this.dgvLeitura.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLeitura.Location = new System.Drawing.Point(72, 139);
+            this.dgvLeitura.Name = "dgvLeitura";
+            this.dgvLeitura.Size = new System.Drawing.Size(473, 174);
+            this.dgvLeitura.TabIndex = 7;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 114F));
+            this.tableLayoutPanel1.Controls.Add(this.lblPercentualDoReajuste, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblTotalComReajuste, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblTotalSemReajuste, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(72, 60);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.58823F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 54.41177F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(386, 56);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // lblPercentualDoReajuste
+            // 
+            this.lblPercentualDoReajuste.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPercentualDoReajuste.AutoSize = true;
+            this.lblPercentualDoReajuste.Location = new System.Drawing.Point(275, 34);
+            this.lblPercentualDoReajuste.Name = "lblPercentualDoReajuste";
+            this.lblPercentualDoReajuste.Size = new System.Drawing.Size(108, 13);
+            this.lblPercentualDoReajuste.TabIndex = 6;
+            this.lblPercentualDoReajuste.Text = "label3";
+            // 
+            // lblTotalComReajuste
+            // 
+            this.lblTotalComReajuste.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalComReajuste.AutoSize = true;
+            this.lblTotalComReajuste.Location = new System.Drawing.Point(139, 34);
+            this.lblTotalComReajuste.Name = "lblTotalComReajuste";
+            this.lblTotalComReajuste.Size = new System.Drawing.Size(130, 13);
+            this.lblTotalComReajuste.TabIndex = 2;
+            this.lblTotalComReajuste.Text = "label2";
+            this.lblTotalComReajuste.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblTotalSemReajuste
+            // 
+            this.lblTotalSemReajuste.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalSemReajuste.AutoSize = true;
+            this.lblTotalSemReajuste.Location = new System.Drawing.Point(3, 34);
+            this.lblTotalSemReajuste.Name = "lblTotalSemReajuste";
+            this.lblTotalSemReajuste.Size = new System.Drawing.Size(130, 13);
+            this.lblTotalSemReajuste.TabIndex = 1;
+            this.lblTotalSemReajuste.Text = "label1";
+            this.lblTotalSemReajuste.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ofdListaFuncionarios
+            // 
+            this.ofdListaFuncionarios.FileName = "ofdListaFuncionarios";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Total Sem Reajuste";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Total Com Reajuste";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Percentual Reajuste";
+            // 
+            // FormLeituraArquivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dgvLeitura);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtArquivo);
+            this.Name = "FormLeituraArquivo";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeitura)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtArquivo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvLeitura;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblPercentualDoReajuste;
+        private System.Windows.Forms.Label lblTotalComReajuste;
+        private System.Windows.Forms.Label lblTotalSemReajuste;
+        private System.Windows.Forms.OpenFileDialog ofdListaFuncionarios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
